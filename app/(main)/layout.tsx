d2d5baa9c4042c1b20ca8f1713b6479e32d84713
Nishant Yadav <nynishuyadav85@gmail.com>
2025-07-03 21:52:22 +0530

    created a rooutr for /book and created a navbar for public who are not logedd inn

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,22 @@
+import PublicNavBar from "@/components/PublicNavBar"
+import { currentUser } from "@clerk/nextjs/server"
+
+export default async function MainLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+
+    const user = await currentUser()
+    return (
+
+
+        <main className="relative">
+            {/* {user ? <PrivateNavBar /> : <PublicNavBar />} */}
+            <PublicNavBar />
+            <section className="pt-36">
+                {children}
+            </section>
+        </main>
+    )
+}
---
+++ components/PublicNavBar.tsx
@@ -0,0 +1,37 @@
+import { SignInButton, SignUpButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+
+const PublicNavBar = () => {
+    return (
+        <nav className='flex justify-between items-center fixed z-50 w-full h-28 bg-gray-300 px-10 gap-4 shadow-2xl'>
+            <Link href={'/login'} className='flex items-center gap-1 hover:scale-150 duration-500'>
+                <Image
+                    src="/asset/logo.svg"
+                    alt='logo'
+                    width={100}
+                    height={100}
+                />
+            </Link>
+
+            <section className='stickey top-0 flex justify-between '>
+                <div className='flex flex-1 max-sm:gap-0 sm:gap-6'>
+                    <SignInButton>
+                        <Button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 cursor-pointer hover:scale-150 duration-500 rounded-2xl shadow-2xl'>
+                            Login
+                        </Button>
+                    </SignInButton>
+                    <SignUpButton>
+                        <Button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 cursor-pointer hover:scale-150 duration-500 rounded-2xl shadow-2xl" variant={"outline"}>
+                            Register
+                        </Button>
+                    </SignUpButton>
+                </div>
+            </section>
+        </nav>
+    )
+}
+
+export default PublicNavBar
