ee7373f1a5e53593ce6900dd8c6d2834166bd682
Nishant Yadav <nynishuyadav85@gmail.com>
2025-07-04 23:47:55 +0530

    created event and PrivateNavBar

--- constants/index.ts
+++ constants/index.ts
@@ -7,3 +7,21 @@ export const DAYS_OF_A_WEEK_IN_ORDER = [
     "saturday",
     "sunday",
 ] as const
+
+export const PrivateNavLinks = [
+    {
+      imgURL: '/asset/events.svg',
+      route: '/events',
+      label: 'My Events',
+    },
+    {
+      imgURL: '/asset/schedule.svg',
+      route: '/schedule',
+      label: 'My Schedule',
+    },
+    {
+      imgURL: '/asset/public.svg',
+      route: '/book',
+      label: 'Public Profile',
+    },
+  ] as const
