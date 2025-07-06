e7f42421545f3c78d202da9589e9230b134ae97c
Nishant Yadav <nynishuyadav85@gmail.com>
2025-07-06 21:32:48 +0530

    Implement EventForm component with form validation schema

--- components/forms/EventForm.tsx
+++ components/forms/EventForm.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client'
 
-const EventForm = () => {
+import { eventFormSchema } from "@/schema/events";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+// component to handle editing/deleting and creating a event
+export default function EventForm({
+    event,
+}: {
+    event?: {
+        id: string,
+        name: string,
+        description?: string,
+        durationInMinutes: number,
+        isActive: boolean
+    }
+}) {
+
+    const form = useForm<z.infer<typeof eventFormSchema>>({
+
+    })
     return (
-        <div>EventForm</div>
-    )
+        <div>
+            {/* Event form implementation goes here */}
+        </div>
+    );
 }
-
-export default EventForm
